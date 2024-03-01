bc5370163f8ff0e21d873e02bcef727895e6f96d
pankaj-kb <pankajb2601@gmail.com>
2024-03-01 14:55:02 +0530

    adding error handler

---
+++ src/utils/APIError.js
@@ -0,0 +1,23 @@
+class APIError extends Error {
+    constructor(
+        statusCode,
+        messsage = "Something went wrong.",
+        errors = [],
+        stack = ""
+    ) {
+        super(messsage),
+            this.statusCode = statusCode
+        this.data = null
+        this.message = messsage
+        this.success = false
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { APIError }
